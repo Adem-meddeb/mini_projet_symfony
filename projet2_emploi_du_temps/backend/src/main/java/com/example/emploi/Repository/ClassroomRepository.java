b6b5f2359de8ba9783852e03e86918f3b5929ee6
Adem meddeb <134943236+Adem-meddeb@users.noreply.github.com>
2024-12-08 13:25:30 +0100

    Add files via upload

---
+++ projet2_emploi_du_temps/backend/src/main/java/com/example/emploi/Repository/ClassroomRepository.java
@@ -0,0 +1,13 @@
+package com.example.emploi.Repository;
+
+import com.example.emploi.entity.Classroom;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClassroomRepository extends JpaRepository<Classroom,Long> {
+
+
+    Classroom findByNumber(int number);
+}
